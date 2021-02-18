95ffec1d6b1e2b4ef81a5b672707a7f78580cea2
ya-tommy <65650249+ya-tommy@users.noreply.github.com>
2021-02-18 09:20:33 -0500

    Saves/loads wanted theme

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function toggleTheme() {
     var theme = document.getElementsByTagName('link')[0];
-    var check = document.getElementById('cb');
     if (theme.getAttribute('href') == 'nightStyle.css') { 
         theme.href = 'style.css';
     } else { 
         theme.href = 'nightStyle.css';
     }
+
+    // Saves the value of checkbox
+    var check = document.getElementById('cb');
+    localStorage.setItem("cb", check.checked);
 }
+
+// Loading the saved value of checkbox
+var checked = JSON.parse(localStorage.getItem("cb"));
+document.getElementById("cb").checked = checked;
 
 var theme = document.getElementsByTagName('link')[0];
 if (document.getElementById('cb').checked ==  true) {
